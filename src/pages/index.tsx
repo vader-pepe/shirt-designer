7c67179b1ed8d8e594e5f0bc5e8b765fda0b2245
chadwithz <kepoya@email.com>
2023-01-23 15:32:51 +0700

    feat: move to src

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
+import { motion } from 'framer-motion'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image, { StaticImageData } from 'next/image'
+import Link from 'next/link'
+
 import bg from '../assets/img/bg1.png'
-import tshirt from '../assets/img/tshirt.png'
 import hoodie from '../assets/img/hoodie.png'
 import mug from '../assets/img/mug.png'
+import tshirt from '../assets/img/tshirt.png'
 import construction from '../assets/img/Under-Construction-Banner.jpg'
-import { motion } from 'framer-motion'
-import Link from 'next/link'
 
 const Home: NextPage = () => {
   const AnimatedStartBtn = ({
@@ -32,7 +33,7 @@ const Home: NextPage = () => {
         transition={{ type: 'spring', stiffness: 400, damping: 10 }}
       >
         <Image src={src} alt={alt} />
-        <small className='absolute bottom-2 right-5 text-white text-2xl'>{title}</small>
+        <small className='absolute bottom-2 right-5 text-2xl text-white'>{title}</small>
       </motion.button>
     )
   }
@@ -47,19 +48,19 @@ const Home: NextPage = () => {
       </Head>
 
       <header className='fixed top-1/3 right-0 left-0 z-20 flex justify-center'>
-        <Image src={construction} alt='website under construction' height={150} className=""/>
+        <Image src={construction} alt='website under construction' height={150} className='' />
       </header>
 
-      <header className='flex justify-center bg-black font-semibold h-12 w-full text-white'>
-        <div style={{ minWidth: '1200px' }} className='flex justify-between items-center'>
-          <Link href={'/'} className='text-xl'>
+      <header className='flex h-12 w-full justify-center bg-black font-semibold text-white'>
+        <div style={{ minWidth: '1200px' }} className='flex items-center justify-between'>
+          <Link href='/' className='text-xl'>
             PDA-TEECRAFT
           </Link>
           <div className='flex gap-1'>
             {/*Red Btn*/}
-            <button className='bg-red py-2 px-3 rounded-md'>Start Creating</button>
+            <button className='rounded-md bg-red py-2 px-3'>Start Creating</button>
             {/*Black Btn*/}
-            <button className='bg-black py-2 px-3 text-white p-2 rounded-md'>Login</button>
+            <button className='rounded-md bg-black p-2 py-2 px-3 text-white'>Login</button>
           </div>
         </div>
       </header>
@@ -68,26 +69,26 @@ const Home: NextPage = () => {
         style={{
           backgroundImage: `url(${bg.src})`,
         }}
-        className='min-h-screen bg-white bg-center bg-no-repeat flex flex-col items-center justify-center text-white relative'
+        className='relative flex min-h-screen flex-col items-center justify-center bg-white bg-center bg-no-repeat text-white'
       >
-        <div className='bg-black opacity-50 absolute inset-0' />
-        <p className='text-5xl z-10'>Design your creativity</p>
-        <p style={{ maxWidth: '975px' }} className='text-center z-10 text-lg'>
+        <div className='absolute inset-0 bg-black opacity-50' />
+        <p className='z-10 text-5xl'>Design your creativity</p>
+        <p style={{ maxWidth: '975px' }} className='z-10 text-center text-lg'>
           Unleash your creativity with our state-of-the-art tshirt design tool. Our user-friendly
           interface allows you to effortlessly customize your own unique designs, featuring a wide
           range of colors, fonts, and graphics.
         </p>
-        <button className='bg-white text-black py-2 px-3 rounded-md z-10 mt-10'>
+        <button className='z-10 mt-10 rounded-md bg-white py-2 px-3 text-black'>
           Start Creating
         </button>
       </main>
 
       <article className='flex flex-col pb-10'>
-        <h1 className='py-10 text-5xl text-center'>Start Creating</h1>
+        <h1 className='py-10 text-center text-5xl'>Start Creating</h1>
         <div className='flex justify-between self-center' style={{ minWidth: '1200px' }}>
-          <AnimatedStartBtn src={tshirt} alt='tshirt start btn' title={'Tshirt'} />
-          <AnimatedStartBtn src={hoodie} alt='hoodie start btn' title={'Hoodie'} />
-          <AnimatedStartBtn src={mug} alt='mug start btn' title={'Mug'} />
+          <AnimatedStartBtn src={tshirt} alt='tshirt start btn' title='Tshirt' />
+          <AnimatedStartBtn src={hoodie} alt='hoodie start btn' title='Hoodie' />
+          <AnimatedStartBtn src={mug} alt='mug start btn' title='Mug' />
         </div>
       </article>
     </div>
