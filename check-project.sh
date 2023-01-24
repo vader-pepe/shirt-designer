5460aa23748a0000316c43137f7591e7addcde21
chadwithz <kepoya@email.com>
2023-01-24 12:41:45 +0700

    fix: bash script

--- check-project.sh
+++ check-project.sh
@@ -3,21 +3,21 @@
 if [ -d "shirt-designer" ]; then
   cd shirt-designer
   echo "----Installing dependencies & building----"
-  bash -c "yarn install"
-  bash -c "yarn build"
+  yarn install
+  yarn build
 else
   echo "----Cloning Projects----"
   git clone git@github.com:chadwithz/shirt-designer.git
   cd shirt-designer
-  git checkout development
+  git checkout -b development origin/development
   echo "----Installing dependencies & building----"
-  bash -c "yarn install"
-  bash -c "yarn build"
+  yarn install
+  yarn build
 fi
 
-bash -c "pm2 list" | grep "shirt-designer"
+pm2 list | grep "shirt-designer"
 if [ $? -eq 0 ]; then
-  bash -c "pm2 stop shirt-designer"
-  bash -c "pm2 delete shirt-designer"
+  pm2 stop "shirt-designer"
+  pm2 delete "shirt-designer"
 fi
 pm2 start npm --name "shirt-designer" -- start
