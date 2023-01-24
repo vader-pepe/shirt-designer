d7155f61e6121d26857053505c38d95164a770bc
chadwithz <kepoya@email.com>
2023-01-24 12:09:27 +0700

    fix: workflows

--- check-project.sh
+++ check-project.sh
@@ -2,7 +2,18 @@
 
 if [ -d "shirt-designer" ]; then
   cd shirt-designer
+  yarn install
+  yarn build
 else
   git clone git@github.com:chadwithz/shirt-designer.git
   cd shirt-designer
+  yarn install
+  yarn build
 fi
+
+pm2 list | grep "shirt-designer"
+if [ $? -eq 0 ]; then
+  pm2 stop "shirt-designer"
+  pm2 delete "shirt-designer"
+fi
+pm2 start npm --name "shirt-designer" -- start
