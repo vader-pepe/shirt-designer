df0f6f62a8faba89d1a44744697057563f10d4bf
chadwithz <kepoya@email.com>
2023-02-02 19:26:08 +0700

    feat: add back construction image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import { Josefin_Sans } from '@next/font/google'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 
 import '../styles/globals.css'
+
+import construction from '../assets/img/Under-Construction-Banner.jpg'
 
 const josefin = Josefin_Sans({ subsets: ['latin'] })
 
@@ -16,10 +19,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         `}
       </style>
       {/*TODO: Remove this while done*/}
-      {/*<div className='fixed inset-0 flex justify-center items-center z-20'>
+      <div className='fixed inset-0 z-20 flex items-center justify-center'>
         <Image src={construction} alt='website under construction' height={150} className='' />
       </div>
-      */}
       <title>Tshirt Designer</title>
       <Component {...pageProps} />
     </>
