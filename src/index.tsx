96b0bc1e685adca1dc332fcefdf5a78e40a3027c
chadwithz <kepoya@email.com>
2023-09-28 02:59:30 +0700

    feat: add register page

--- src/index.tsx
+++ src/index.tsx
@@ -11,23 +11,53 @@ const BaseDoc = ({ children }: elements.Children) => `<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="./public/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   </head>
-${children}
+  ${children}
 <script src="./public/htmx.min.js"></script>
 <script src="./public/hyperscript.min.js"></script>
-<script src="./public/dynamics.min.js"></script>
 </html>`
 
-const AnimBtn = ({ src, alt, title }: { src: string; alt: string; title: string; }) => {
+const AnimSelectBtn = ({ src, alt, title }: { src: string; alt: string; title: string; }) => {
   return <button
-    style="transition: all 800ms ease-in"
+    style="transition: all 100ms cubic-bezier(.68,-0.55,.27,1.55);"
     class='relative'
-    _="on mouseenter transition my scale to 1.05 on mouseleave transition my scale to initial"
+    _="on mouseenter add .scale-105 then settle then on mouseleave remove .scale-105"
   >
     <div class="absolute w-full h-full bg-black opacity-30 rounded-lg"></div>
     <img src={src} alt={alt} />
     <small class='absolute bottom-2 right-5 text-2xl text-white'>{title}</small>
   </button>
+}
+
+const AnimBtn = ({ children, className }: { children: string | JSX.Element; className?: string; }) => {
+  return <button
+    style="transition: all 100ms cubic-bezier(.68,-0.55,.27,1.55);"
+    class={className ? className : ''}
+    _="on mouseenter add .scale-105 then settle then on mouseleave remove .scale-105"
+  >
+    {children}
+  </button>
+}
+
+const Header = ({ pos = 'main' }: { pos?: 'main' | 'login' }) => {
+  return <header class={`z-10 ${pos === 'main' ? 'bg-black' : ''} py-2 px-4`}>
+    <div class='flex items-center justify-between max-w-[1200px] m-[auto]'>
+      <a href='/'>
+        <h3 class={`${pos === 'main' ? 'text-white' : 'text-black'}`}>PDA-TEECRAFT</h3>
+      </a>
+      <div class='flex gap-2'>
+        <AnimBtn className={`rounded-md ${pos === 'main' ? 'bg-red-700' : 'bg-black'} py-2 px-3 text-white hover:opacity-90`}>
+          <a href="/register">{pos === 'main' ? 'Start Creating' : 'Create Account'}</a>
+        </AnimBtn>
+        <AnimBtn className={`hidden sm:block rounded-md bg-black py-1 px-2 text-white hover:opacity-90 ${pos === 'main' ? '' : 'sm:hidden'}`}>
+          <a href="/login" >
+            Login
+          </a>
+        </AnimBtn>
+      </div>
+    </div>
+  </header>
 }
 
 const app = new Elysia()
@@ -36,27 +66,9 @@ const app = new Elysia()
   .get("/", ({ html }) => html(
     <BaseDoc>
       <body class="font-semibold">
-        <div
-          class='flex relative min-h-screen flex-col bg-cover bg-center bg-[url(./assets/bg1.png)]'
-        >
+        <div class='flex relative min-h-[950px] flex-col bg-cover bg-center bg-[url(./assets/bg1.png)]' >
           <div class="absolute inset-0 bg-black opacity-70"></div>
-          <header class='z-10 bg-black py-2 px-4'>
-            <div
-              class='flex items-center justify-between max-w-[1200px] m-[auto]'
-            >
-              <a href='/'>
-                <h3 class='text-white'>PDA-TEECRAFT</h3>
-              </a>
-              <div class='flex gap-2'>
-                <button class='rounded-md bg-red-700 py-2 px-3 text-white hover:opacity-90'>
-                  Start Creating
-                </button>
-                <button class='rounded-md bg-black py-1 px-2 text-white hover:opacity-90'>
-                  Login
-                </button>
-              </div>
-            </div>
-          </header>
+          <Header></Header>
 
           <div class='border-box z-10 mx-8 mt-10 flex flex-col items-center justify-center rounded-md bg-black/70 p-4 backdrop-blur-sm md:hidden'>
             <div class='w-10 text-orange-500' />
@@ -68,7 +80,7 @@ const app = new Elysia()
 
           <main class='z-10'>
             <h2 class='mt-20 text-center text-3xl text-white md:mt-36 md:text-5xl'>
-              Design your creativity
+              Design your creativity !
             </h2>
 
             <p
@@ -80,25 +92,133 @@ const app = new Elysia()
             </p>
 
             <div class=' mt-10 flex items-center justify-center md:mt-36'>
-              <button class='py-2 px-3 text-md rounded-md bg-white py-1 px-2 hover:opacity-90 '>
+              <AnimBtn className='py-2 px-3 text-md rounded-md bg-white py-1 px-2 hover:opacity-90 '>
                 Start Creating
-              </button>
+              </AnimBtn>
             </div>
           </main>
         </div>
         <article class='relative hidden flex-col pb-10 md:flex'>
           <h1 class='py-10 text-center text-5xl'>Start Creating</h1>
           <div class='flex gap-5 justify-between self-center max-w-[1200px]'>
-            <AnimBtn src="./public/assets/tshirt.png" title="Tshirt" alt="tshirt button" />
-            <AnimBtn src="./public/assets/hoodie.png" title="Hoodie" alt="hoodie button" />
-            <AnimBtn src="./public/assets/mug.png" title="Mug" alt="mug button" />
+            <AnimSelectBtn src="./public/assets/tshirt.png" title="Tshirt" alt="tshirt button" />
+            <AnimSelectBtn src="./public/assets/hoodie.png" title="Hoodie" alt="hoodie button" />
+            <AnimSelectBtn src="./public/assets/mug.png" title="Mug" alt="mug button" />
           </div>
           <div class="z-[-1] absolute inset-0 bg-black opacity-10"></div>
         </article>
       </body>
     </BaseDoc >
   ))
-  .post("/clicked", () => <div>Im from server</div>)
+  .get("/login", ({ html }) => html(
+    <BaseDoc>
+      <body class="font-semibold">
+        <div class="flex min-h-[950px] flex-col">
+          <Header pos="login"></Header>
+          <div class="pt-20 grid grid-cols-1 place-items-center px-64">
+            <h3 class="text-3xl font-medium">Login to TEECRAFT</h3>
+
+            <div class="min-w-[400px] mt-20">
+              <label class="flex flex-col text-xl">
+                <small>Email</small>
+                <input class="rounded-md py-2 px-3 border border-1 font-light" placeholder="Input your email address" />
+              </label>
+            </div>
+
+            <div class="min-w-[400px] mt-5 relative">
+              <label class="flex flex-col text-xl">
+                <small>Password</small>
+                <input _="on changevis if @type is 'password' set @type to 'text' otherwise set @type to 'password'" class="rounded-md py-2 pl-3 pr-10 border border-1 font-light" type="password" placeholder="Input your password" />
+                <button _="on click send changevis to <input,i/>">
+                  <i _="on changevis if @class contains 'fa-eye-slash' remove .fa-eye-slash then add .fa-eye otherwise remove .fa-eye then add .fa-eye-slash" class="fa-solid fa-eye absolute bottom-3 right-2 text-gray-700"></i>
+                </button>
+              </label>
+            </div>
+
+            <div class="min-w-[400px] mt-20">
+              <AnimBtn className="rounded-md bg-black text-center text-white w-full py-2 px-3">Login</AnimBtn>
+            </div>
+
+            <div class="min-w-[400px] mt-10">
+              <a class="underline-offset-1 underline" href="#">Forgot Password?</a>
+            </div>
+            <div class="min-w-[400px] mt-2">
+              <a class="underline-offset-1 underline" href="/register">New User?</a>
+            </div>
+
+            <div class="min-w-[400px] mt-14 text-center">
+              &boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&nbsp;or&nbsp;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;
+            </div>
+
+          </div>
+
+        </div>
+      </body>
+    </BaseDoc>
+  ))
+  .get("/register", ({ html }) => html(
+    <BaseDoc>
+      <body class="font-semibold">
+        <div class="min-h-[950px] flex">
+          <img src='./public/assets/bg2.png' alt="background 2" class="min-w-[600px] hidden lg:block bg-cover" />
+          <div class="w-full">
+            <header class="flex justify-end px-5 py-3 font-light text-xl">
+              Already have an account? <a class="underline underline-offset-1" href="/login"> Login</a>
+            </header>
+
+            <main class="flex flex-col items-center w-full">
+              <h3 class="text-3xl">Unleash your creativity!</h3>
+              <div class="min-w-[400px] mt-20">
+                <label class="flex flex-col text-xl">
+                  <small>Fullname</small>
+                  <input class="rounded-md py-2 px-3 border border-1 font-light" placeholder="Input your fullname" />
+                </label>
+              </div>
+
+              <div class="min-w-[400px] mt-5">
+                <label class="flex flex-col text-xl">
+                  <small>Email</small>
+                  <input class="rounded-md py-2 px-3 border border-1 font-light" placeholder="Input your email address" />
+                </label>
+              </div>
+
+              <div class="min-w-[400px] mt-5 relative">
+                <label class="flex flex-col text-xl">
+                  <small>Password</small>
+                  <input _="on changevis if @type is 'password' set @type to 'text' otherwise set @type to 'password'" class="rounded-md py-2 pl-3 pr-10 border border-1 font-light" type="password" placeholder="Input your password" />
+                  <button _="on click send changevis to <input,i/>">
+                    <i _="on changevis if @class contains 'fa-eye-slash' remove .fa-eye-slash then add .fa-eye otherwise remove .fa-eye then add .fa-eye-slash" class="fa-solid fa-eye absolute bottom-3 right-2 text-gray-700"></i>
+                  </button>
+                </label>
+              </div>
+
+              <div class="min-w-[400px] mt-5 relative">
+                <label class="flex flex-col text-xl">
+                  <small>Retype Password</small>
+                  <input _="on changevis2 if @type is 'password' set @type to 'text' otherwise set @type to 'password'" class="rounded-md py-2 pl-3 pr-10 border border-1 font-light" type="password" placeholder="Input your password" />
+                  <button _="on click send changevis2 to <input,i/>">
+                    <i _="on changevis2 if @class contains 'fa-eye-slash' remove .fa-eye-slash then add .fa-eye otherwise remove .fa-eye then add .fa-eye-slash" class="fa-solid fa-eye absolute bottom-3 right-2 text-gray-700"></i>
+                  </button>
+                </label>
+              </div>
+
+              <div class="min-w-[400px] mt-20">
+                <AnimBtn className="rounded-md bg-black text-center text-white w-full py-2 px-3">Register</AnimBtn>
+              </div>
+
+              <div class="min-w-[400px] mt-5 font-light text-center">
+                Already have an account? <a class="underline underline-offset-1" href="/login"> Login</a>
+              </div>
+              <div class="min-w-[400px] mt-14 text-center font-light">
+                &boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&nbsp;or&nbsp;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;&boxh;
+              </div>
+
+            </main>
+          </div>
+        </div>
+      </body>
+    </BaseDoc>
+  ))
   .listen(3000);
 
 console.log(
